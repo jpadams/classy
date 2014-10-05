aa4342e99fdc3c743256e87138e093115321c1f2
Jeremy Adams <jeremy@puppetlabs.com>
2014-10-04 22:21:34 -0700

    add comments

--- rule.sh
+++ rule.sh
@@ -1,11 +1,21 @@
+# This could be a great demo of the NC API. Change the rule in the PE MCollective group
+# in the GUI. This script will print the current rule and then update it and print it again.
+# You could then refesh the GUI to view the updated rule.
+
+# This hackily extracts the group ID of the "PE MCollective" group from the NC
+# ToDo: rewrite this
 PEMCoID=$(curl --cacert `puppet agent --configprint localcacert` --cert `puppet agent --configprint hostcert` --key `puppet agent --configprint hostprivkey` --insecure https://localhost:4433/classifier-api/v1/groups| python -m json.tool |grep -C 2 "PE MCollective" | grep "id" | cut -d: -f2 | sed 's/[\", ]//g')
 
 echo "####################################"
 
+# This will pretty print the JSON of the "PE MCollective" group in the NC
 curl -k --cacert `puppet agent --configprint localcacert` --cert `puppet agent --configprint hostcert` --key `puppet agent --configprint hostprivkey` --insecure https://localhost:4433/classifier-api/v1/groups/$PEMCoID | python -m json.tool
 
 echo "####################################"
 
+# This will update the matching rule of the "PE MCollective" group and print resulting JSON
+# note: "00000000-0000-4000-8000-000000000000" is the root group
+# https://github.com/puppetlabs/classifier/blob/master/doc/api/v1/groups.markdown#post-v1groups
 curl -X POST -H 'Content-Type: application/json' \
 -d \
 '{
