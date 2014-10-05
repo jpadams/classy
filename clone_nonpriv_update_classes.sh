40176898500ed09f14995616048a1a9aac110ce5
Jeremy Adams <jeremy@puppetlabs.com>
2014-10-05 06:50:58 -0700

    add popd

--- clone_nonpriv_update_classes.sh
+++ clone_nonpriv_update_classes.sh
@@ -8,3 +8,5 @@ echo "####################################"
 
 curl -X POST -H 'Content-Type: application/json' \
 --cacert `/opt/puppet/bin/puppet agent --configprint localcacert` --cert `/opt/puppet/bin/puppet agent --configprint hostcert` --key `/opt/puppet/bin/puppet agent --configprint hostprivkey` --insecure https://localhost:4433/classifier-api/v1/update-classes | python -m json.tool
+
+popd
