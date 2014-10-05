96ca13eadb606cb17011275ae4cb264844b48c76
Jeremy Adams <jeremy@puppetlabs.com>
2014-10-05 07:41:25 -0700

    add /vagrant/.. prefix

--- setup_nonpriv.sh
+++ setup_nonpriv.sh
@@ -1,3 +1,3 @@
-clone_nonpriv_update_classes.sh
-classify_default_with_nonpriv.sh
-apply_rule_using_isadmin_fact.sh
+/vagrant/scripts/classy/clone_nonpriv_update_classes.sh
+/vagrant/scripts/classy/classify_default_with_nonpriv.sh
+/vagrant/scripts/classy/apply_rule_using_isadmin_fact.sh
